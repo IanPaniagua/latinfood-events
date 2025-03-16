911dcf666f842c5dbc47c52167fba3156a4b7083
IanPaniagua <paniagua.ian.de@gmail.com>
2025-03-16 13:03:18 +0100

    PresentatinCard Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,13 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import PresentationCard from "@/components/PresentationCard";
+
 
 
 export default function Home() {
   return (
-    <div className="bg-[url(/images/food-dark.jpg)] bg-cover bg-center">
-    <main className="flex flex-col items-center justify-center text-center max-w-5xl mx-auto h-dvh">
-
-      <div className="flex flex-col gap-6 p-12 rounded-xl bg-linear-65 from-green-0 to-yellow-200 w-4/5 sm:max-w-96 mx-auto text-black sm:text-2xl" >
-
-        <h1 className="text-4xl font-bold">Latin Food Events<br />Hamburg</h1>
-      <address>
-        24092 Hamburg <br />
-        Germany <br />
-      </address>
-
-      <Link href="tel:5555555555" className="hover:underline" >
-        555-555-5555
-      </Link>
-      <Link href="/events">
-        <Button>More info</Button>
-      </Link>
-      </div>
-
+    <div className="bg-[url(/images/food-dark.jpg)] bg-cover bg-center min-h-dvh">
+    <main className="flex flex-col items-center justify-center  text-center max-w-5xl mx-auto py-8">
+      <PresentationCard />
     </main>
-
   </div>
   );
 }
---
+++ components/PresentationCard.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function PresentationCard() {
+
+    return (
+        <div className="flex flex-col gap-6 p-12 rounded-xl bg-linear-65 from-green-0 to-yellow-200 w-4/5 sm:max-w-96 mx-auto text-black sm:text-2xl" >
+            <h1 className="text-4xl font-bold">Latin Food</h1>
+            <p className="text-xl">Nuvia Lopez</p>
+            <Image 
+                src="/images/profile.jpg"
+                alt="Nuvia's profile picture"
+                width={250}
+                height={300}
+                className="rounded-xl object-cover mx-auto"
+                priority
+            />
+            {/* tipo de comida */}
+            <p>Yo preparo comida de Ecuador, especialista en pescado y arroces</p>
+            <address>
+                24092 Hamburg <br />
+                Germany <br />
+            </address>
+
+            <Link href="tel:5555555555" className="hover:underline" >
+                555-555-5555
+            </Link>
+            <Link href="/events">
+                <Button>My Next Events</Button>
+            </Link>
+            <p>Me necesitas para tu proximo evento ?</p>
+            <Link href="/events">
+                <Button>Contratar Servicio</Button>
+            </Link>
+      </div>
+    )
+}
